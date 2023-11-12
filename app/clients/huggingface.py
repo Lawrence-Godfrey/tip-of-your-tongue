52444873d7db59b4962e4f9d9e8dedd0f633ddce
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2023-11-12 18:56:59 +0200

    Refactor classes and move functionality to super-classes. Improve typing.

--- app/clients/huggingface.py
+++ app/clients/huggingface.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Union
+from urllib.parse import urljoin
 
 import requests
 
@@ -8,14 +9,19 @@ class HuggingFaceClient:
     Client for interacting with the HuggingFace inference API.
     """
 
-    def __init__(self, api_url: str, api_key: str):
+    def __init__(self, api_url: str, api_key: str, model_name: str):
         self.api_url = api_url
         self.api_key = api_key
+        self.model_name = model_name
+
+        self.endpoint = urljoin(self.api_url, self.model_name)
 
     def get_headers(self):
         return {"Authorization": f"Bearer {self.api_key}"}
 
-    def predict_word(self, sentence: str or List[str]) -> List[tuple[str, float]]:
+    def predict_word(
+            self, sentence: Union[str, List[str]]
+    ) -> Union[List[dict], List[List[dict]]]:
         """
         Predict the most likely word to fill in the blank for each sentence.
 
@@ -23,8 +29,8 @@ class HuggingFaceClient:
             sentence: A sentence or list of sentences with a blank in it, represented by '[MASK]'.
 
         Returns:
-            A list of tuples containing the predicted word and its probability.
+            A list ( or list of lists) of tuples containing the predicted word and its probability.
         """
 
-        response = requests.post(self.api_url, headers=self.get_headers(), json={"inputs": sentence})
+        response = requests.post(self.endpoint, headers=self.get_headers(), json={"inputs": sentence})
         return response.json()
